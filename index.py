750ba2ff65a06462015934f53bc568ee0c32dbdd
Sony <schk9611@gmail.com>
2022-05-24 22:30:14 +0900

    add update part

--- index.py
+++ index.py
@@ -1,34 +1,39 @@
-#! /usr/local/bin/python3
-import cgi
+#!/usr/local/bin/python3
 import os
+import cgi
 print("Content-Type: text/html")
 print()
+
 files = os.listdir('data')
 listStr = ''
 for item in files:
     listStr = listStr + \
         '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+
 form = cgi.FieldStorage()
 if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
+    update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
 else:
     pageId = 'Welcome'
     description = 'Hello, web'
-print('''<!DOCTYPE html>
-<html lang="en">
+    update_link = ''
+print('''<!doctype html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>WEB1</title>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
 </head>
 <body>
-    <h1><a href="index.py">WEB</a></h1>
-    <ol>
-        {listStr}
-    </ol>
-    <a href="create.py">create</a>
-    <h2>{title}</h2>
-    <p>{desc}</p>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="create.py">create</a>
+  {update_link}
+  <h2>{title}</h2>
+  <p>{desc}</p>
 </body>
-</html>'''
-      .format(title=pageId, desc=description, listStr=listStr))
+</html>
+'''.format(title=pageId, desc=description, listStr=listStr, update_link=update_link))
