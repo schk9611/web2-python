b550a20ae2fc3868a9b6be10ebeb97152c2b0bfd
Sony <schk9611@gmail.com>
2022-05-24 13:18:45 +0900

    python3 type String part2

---
+++ syntax/string2.py
@@ -0,0 +1,16 @@
+a = 'Hello Python'
+print(a)
+
+# 문자열의 길이(length)
+print(len(a))
+
+# 문자열 위치(index)
+print(a[0])
+print(a[1])
+print(a[2])
+
+# 문자열 슬라이싱(slicing)
+print(a[2:4])
+
+# repeat
+print((a+'\n')*2)
