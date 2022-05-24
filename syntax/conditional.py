a65414a598ca31d4b4147b0b329c3b5d7d81a602
Sony <schk9611@gmail.com>
2022-05-24 16:14:22 +0900

    python3 conditional

---
+++ syntax/conditional.py
@@ -0,0 +1,17 @@
+user_id = input('id?')
+user_pwd = input('password?')
+
+'''
+if user_input == '111111':
+    print('Hello master')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'sony':
+    if user_pwd == '111111':
+        print('Hello master')
+    else:
+        print('Who are you?')
+else:
+    print('Who are you?')
