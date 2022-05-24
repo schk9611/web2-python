2d2e4c25378431b822b89ffee929602d29ef595e
Sony <schk9611@gmail.com>
2022-05-24 16:19:57 +0900

    python3 logical operator

--- syntax/conditional.py
+++ syntax/conditional.py
@@ -8,10 +8,19 @@ else:
     print('Who are you?')
 '''
 
+'''
 if user_id == 'sony':
     if user_pwd == '111111':
         print('Hello master')
     else:
         print('Who are you?')
+else:
+    print('Who are you?')
+'''
+
+if user_id == 'sony' and user_pwd == '111111':
+    print('Hello master')
+elif user_id == 'chan' and user_pwd == '222':
+    print('Hello master')
 else:
     print('Who are you?')
