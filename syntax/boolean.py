9da6afe58bb1cd476de1f1719106ec1463c5014e
Sony <schk9611@gmail.com>
2022-05-24 16:04:02 +0900

    python3 type Boolean

---
+++ syntax/boolean.py
@@ -0,0 +1,18 @@
+# Number
+import os.path
+print(1)   # Integer
+# String
+print('Hello world')
+# Boolean
+print(True)   # 참, 1
+print(False)   # 거짓, 0
+# Expression
+print(1+1)   # 2
+print('Hello '+'world')   # Hello world
+# Comparison operator : 비교 연산자
+print(1 == 1)   # True
+print(1 < 2)   # True
+print(2 < 1)   # False
+# Membership operator
+print('world' in 'Hello world')   # True
+print(os.path.exists('boolean2.py'))
