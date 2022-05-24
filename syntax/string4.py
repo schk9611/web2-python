faad90fe3ce9d1b7ff39f0332c72052f4641d668
Sony <schk9611@gmail.com>
2022-05-24 13:42:55 +0900

    python3 type String format

---
+++ syntax/string4.py
@@ -0,0 +1,8 @@
+# formating 포맷팅
+# 코드가 많아지고 프로젝트가 많아지면 많은 어려움이 생길 수 있다. 굳이 이렇게 까지 라는 생각이 들면 극단적으로 생각해보자.
+
+# positional formating
+print('to {}, dnkefn dvmdl lkne dcod {} wpeoe zausid eiwjen cvje aice {} davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv {} qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein'.format('name', 12, 'name', 'name'))
+
+# named placeholder
+print('to {name}, dnkefn dvmdl lkne dcod {age} wpeoe zausid eiwjen cvje aice {name} davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv {name} qpeein davne cizjv qpeein davne cizjv qpeein davne cizjv qpeein'.format(name='hendy', age=12))
