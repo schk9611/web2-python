2d1f769549a1f0e90589ed735436e71d7df31daa
Sony <schk9611@gmail.com>
2022-05-25 12:33:47 +0900

    python3 function

---
+++ syntax/func.py
@@ -0,0 +1,43 @@
+# code...
+a = 1
+b = 2
+c = 3
+s = a+b+c
+r = s/3
+print(r)
+# code...
+
+
+# function
+def average():
+    a = 1
+    b = 2
+    c = 3
+    s = a+b+c
+    r = s/3
+    print(r)
+
+
+average()
+
+
+# input
+# parameter
+# argument
+def average(a, b, c):
+    s = a+b+c
+    r = s/3
+    print(r)
+
+
+average(10, 20, 30)
+
+
+# return
+def average(a, b, c):
+    s = a+b+c
+    r = s/3
+    return r
+
+
+print(average(10, 20, 30))
